20317d674b60b30fedc158b12817a7e1b015303c
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-11 13:19:49 -0500

    Update meanmedian.py
    Guided Practice

--- meanmedian.py
+++ meanmedian.py
@@ -3,3 +3,22 @@
 #Calculate and display the mean and median
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
+
+
+thisIsAList = ["hello", "world"]
+thisIsATuple = ("hello", "world")
+
+thisIsADictionary = {"word1": "hello", "word2": "world"} 
+thisIsASet = {"hello", "world"}
+
+print("line A: " + thisIsAList[0] + "," + thisIsATuple[1])
+
+print("line B: " + thisIsADictionary["word1"] + ", " + thisIsADictionary["word2"])
+
+theString = ""
+for word in thisIsASet:
+    if word == "hello": 
+        theString = word
+    else: theString = theString + ", " + word
+
+print("line C: " + theString)
